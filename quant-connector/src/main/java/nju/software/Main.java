5195d57721eea3f8ce1021fed7d29908671a3933
hanxiangyu <201250096@smail.nju.edu.cn>
2025-12-11 17:42:32 +0800

    Initial project structure

---
+++ quant-connector/src/main/java/nju/software/Main.java
@@ -0,0 +1,11 @@
+package nju.software;
+
+/**
+ * @author Han
+ * @Description TODO
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
